1fc248cc001bc7391749212903f6e64b4892af49
orhan <oerdayimm@gmail.com>
2020-11-22 19:25:13 +0300

    login bitti

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,20 +4,30 @@
     <div style="margin-bottom: 100px;"></div>
     <main class="py-4">
         <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-md-8">
+            <div class="row">
+                <div class="col-md-5">
+                    <div style="text-align: center;">
+                        <h2>&nbsp;</h2>
+                    </div>
+                    <img src="https://via.placeholder.com/400X200">
+                </div>
+
+                <div class="col-md-7 ml-auto mr-8">
 
                     <div style="text-align: center;"><h2>{{ __('Login') }}</h2></div>
 
                     <div class="card-body">
-                        <form method="POST"action="{{ route('login') }}">
+                        <form method="POST" action="{{ route('login') }}">
                             @csrf
 
                             <div class="form-group row">
-                                <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                                <label for="email"
+                                       class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                                 <div class="col-md-6">
-                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                    <input id="email" type="email"
+                                           class="form-control @error('email') is-invalid @enderror" name="email"
+                                           value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                     @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -28,10 +38,13 @@
                             </div>
 
                             <div class="form-group row">
-                                <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                                <label for="password"
+                                       class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                                 <div class="col-md-6">
-                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                    <input id="password" type="password"
+                                           class="form-control @error('password') is-invalid @enderror" name="password"
+                                           required autocomplete="current-password">
 
                                     @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -43,7 +56,8 @@
 
                             <div class="form-group row mb-0">
                                 <div class="col-md-8 offset-md-4">
-                                    <button type="submit" class="btn btn-outline-danger mb-2 mr-2">{{ __('Login') }}</button>
+                                    <button type="submit"
+                                            class="btn btn-outline-danger mb-2 mr-2">{{ __('Login') }}</button>
                                 </div>
                             </div>
                         </form>
