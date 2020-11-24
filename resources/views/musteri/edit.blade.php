3b2c3470ca968fbe314964c4e22f55860eaa934f
orhan <oerdayimm@gmail.com>
2020-11-24 11:02:26 +0300

    uyarı onay ekranı

--- resources/views/musteri/edit.blade.php
+++ resources/views/musteri/edit.blade.php
@@ -7,13 +7,25 @@
             <div class="row justify-content-center">
                 <div class="col-md-12">
                     <div class="card">
-
+                        @if(\App\User::find(Auth::id())->durum == 0 )
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <div class="alert alert-danger alert-dismissible">
+                                        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                                        <h4><i class="icon fa fa-ban"></i> Uyarı!</h4>
+                                        Firmanız henüz onaylanmadı. Firmanızın bilgilerini doldurduktan sonra, en kısa sürede firmanız onaylanacaktır
+                                    </div>
+                                </div>
+                            </div>
+                        @endif
                         <form action="{{route('musteri.update')}}" method="post" autocomplete="off"
                               enctype="multipart/form-data">
                             {{csrf_field()}}
                             <div class="card-header">
                                 <h3 class="card-title"><strong>Firma</strong> Bilgisi Düzenleme</h3>
                             </div>
+
+
                             <div class="card-body">
                                 <div class="row">
 
