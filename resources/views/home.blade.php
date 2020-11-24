94995459ac0d929de53f47657525785da92773e1
orhan <oerdayimm@gmail.com>
2020-11-24 11:03:21 +0300

    uyarı onay ekranı

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,17 @@
         <!-- Content Header (Page header) -->
         <section class="content-header">
             <div class="container-fluid">
+                @if(\App\User::find(Auth::id())->durum == 0 )
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="alert alert-danger alert-dismissible">
+                            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                            <h4><i class="icon fa fa-ban"></i> Uyarı!</h4>
+                            Firmanız henüz onaylanmadı. Firmanızın bilgilerini doldurduktan sonra, en kısa sürede firmanız onaylanacaktır
+                        </div>
+                    </div>
+                </div>
+                @endif
                 <div class="row">
                     <div class="col-lg-3 col-6">
                         <!-- small box -->
