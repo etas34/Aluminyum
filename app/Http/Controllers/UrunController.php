1731c467a964410a664277d8b459c6ce473e8c92
orhan <oerdayimm@gmail.com>
2020-11-26 14:27:14 +0300

    Güvenlik açığı düzeltildi

--- app/Http/Controllers/UrunController.php
+++ app/Http/Controllers/UrunController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Urun;
 use App\User;
+use Auth;
 use Illuminate\Http\Request;
 
 class UrunController extends Controller
@@ -15,8 +16,8 @@ class UrunController extends Controller
      */
     public function index()
     {
-        $urun = Urun::where('user_id','=',\Auth::id())->get();
-        return view('urun.index',compact('urun'));
+        $urun = Urun::where('user_id', '=', \Auth::id())->get();
+        return view('urun.index', compact('urun'));
     }
 
     /**
@@ -33,7 +34,7 @@ class UrunController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -41,35 +42,33 @@ class UrunController extends Controller
         $urun = new Urun();
 
 
+        if ($request->file('foto')) {
 
-        if ($request->file('foto'))
-        {
+            $image = $request->file('foto');
+            $ext = $image->extension();
+            $image_name = 'logo' . time() . "." . $ext;
+            $upload_path = 'urun/';
+            $image_url = "storage/app/" . $upload_path . $image_name;
 
-            $image=$request->file('foto');
-            $ext=$image->extension();
-            $image_name='logo'.time().".".$ext;
-            $upload_path='urun/';
-            $image_url="storage/app/".$upload_path.$image_name;
+            $image->storeAs($upload_path, $image_name);
 
-            $image->storeAs($upload_path,$image_name);
-
-            $urun->foto=url($image_url);
+            $urun->foto = url($image_url);
 
         }
         $urun->user_id = \Auth::id();
         $urun->ad = $request->ad;
-        $urun->aciklama	= $request->aciklama;
+        $urun->aciklama = $request->aciklama;
         $saved = $urun->save();
 
         if ($saved)
-            $notification=array(
-                'messege'=>'Kayıt Başarılı',
-                'alert-type'=>'success'
+            $notification = array(
+                'messege' => 'Kayıt Başarılı',
+                'alert-type' => 'success'
             );
         else
-            $notification=array(
-                'messege'=>'Dikkat ! Bir Hata Oluştu',
-                'alert-type'=>'error'
+            $notification = array(
+                'messege' => 'Dikkat ! Bir Hata Oluştu',
+                'alert-type' => 'error'
             );
         return redirect()->route('urun.index')->with($notification);
 
@@ -79,7 +78,7 @@ class UrunController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\Urun  $urun
+     * @param \App\Urun $urun
      * @return \Illuminate\Http\Response
      */
     public function show(Urun $urun)
@@ -90,81 +89,93 @@ class UrunController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Urun  $urun
+     * @param \App\Urun $urun
      * @return \Illuminate\Http\Response
      */
     public function edit(Urun $urun)
     {
-        return view('urun.edit',compact('urun'));
+        if (Auth::id() == $urun->user_id)
+            return view('urun.edit', compact('urun'));
+        return abort(404);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Urun  $urun
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Urun $urun
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Urun $urun)
     {
+        if (Auth::id() == $urun->user_id) {
 
+            if ($request->file('foto')) {
 
-        if ($request->file('foto'))
-        {
+                $image = $request->file('foto');
+                $ext = $image->extension();
+                $image_name = 'logo' . time() . "." . $ext;
+                $upload_path = 'urun/';
+                $image_url = "storage/app/" . $upload_path . $image_name;
 
-            $image=$request->file('foto');
-            $ext=$image->extension();
-            $image_name='logo'.time().".".$ext;
-            $upload_path='urun/';
-            $image_url="storage/app/".$upload_path.$image_name;
+                $image->storeAs($upload_path, $image_name);
 
-            $image->storeAs($upload_path,$image_name);
+                $urun->foto = url($image_url);
 
-            $urun->foto=url($image_url);
+            }
+            $urun->ad = $request->ad;
+            $urun->aciklama = $request->aciklama;
+            $saved = $urun->save();
+
+            if ($saved)
+                $notification = array(
+                    'messege' => 'Kayıt Başarılı',
+                    'alert-type' => 'success'
+                );
+            else
+                $notification = array(
+                    'messege' => 'Dikkat ! Bir Hata Oluştu',
+                    'alert-type' => 'error'
+                );
+            return redirect()->route('urun.index')->with($notification);
 
         }
-        $urun->ad = $request->ad;
-        $urun->aciklama	= $request->aciklama;
-        $saved = $urun->save();
-
-        if ($saved)
-            $notification=array(
-                'messege'=>'Kayıt Başarılı',
-                'alert-type'=>'success'
-            );
         else
-            $notification=array(
-                'messege'=>'Dikkat ! Bir Hata Oluştu',
-                'alert-type'=>'error'
-            );
-        return redirect()->route('urun.index')->with($notification);
+            return abort(404);
+
+
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Urun  $urun
+     * @param \App\Urun $urun
      * @return \Illuminate\Http\Response
      */
     public function destroy(Urun $urun)
     {
-        if ($urun->foto){
-            $path_parts = pathinfo($urun->foto);
-            $deleted = unlink(storage_path('app/urun/'.$path_parts['basename']));
+        if (Auth::id() == $urun->user_id) {
+            if ($urun->foto) {
+                $path_parts = pathinfo($urun->foto);
+                $deleted = unlink(storage_path('app/urun/' . $path_parts['basename']));
+            }
+
+
+            $saved = $urun->delete();
+            if ($saved)
+                $notification = array(
+                    'messege' => 'Silme Başarılı',
+                    'alert-type' => 'success'
+                );
+            else
+                $notification = array(
+                    'messege' => 'Dikkat ! Bir Hata Oluştu',
+                    'alert-type' => 'error'
+                );
+            return back()->with($notification);
         }
-
-
-        $saved = $urun->delete();
-        if ($saved)
-            $notification=array(
-                'messege'=>'Silme Başarılı',
-                'alert-type'=>'success'
-            );
-        else
-            $notification=array(
-                'messege'=>'Dikkat ! Bir Hata Oluştu',
-                'alert-type'=>'error'
-            );
-        return back()->with($notification);
+        return abort(404);
     }
+
+
 }
